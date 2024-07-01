6d1baa292f5735bd617a6cb3947a65aae0e192b7
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-07-01 21:58:25 +0100

    formatted the code properly and implemented navigation for laptop screens

--- Script.js
+++ Script.js
@@ -2,12 +2,12 @@ const currentTimeUTC = document.getElementById("currentTimeUTC");
 const currentDayElement = document.getElementById("currentDay");
 
 function updateTimeAndDay() {
-  const date = new Date();
-  currentTimeUTC.textContent = date.toLocaleString("en-US", { timeZone: "UTC" });
+    const date = new Date();
+    currentTimeUTC.textContent = date.toLocaleString("en-US", { timeZone: "UTC" });
 
-  const day = date.getDay(); // Get the day of the week (0-6)
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  currentDayElement.textContent = ` ${daysOfWeek[day]}, `;
+    const day = date.getDay(); // Get the day of the week (0-6)
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    currentDayElement.textContent = ` ${daysOfWeek[day]}, `;
 }
 
 updateTimeAndDay();
